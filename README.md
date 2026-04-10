6f587cf57f41c46625509cd59faea0400eaa045e
Gaelle2608 <mgfelicite@student.udm.ac.mu>
2026-04-10 11:46:30 +0400

    Partie 3 – balayage serpentin

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # turtle_scanner_gaelle1
 ![spawn target](image/image1.png)
 
+## Balayage serpentin
+
+- Kp_lin = 1.0
+- Kp_ang = 4.0
+
+### Effet des gains :
+- Trop grand → oscillations / instabilité
+- Trop petit → mouvement lent et inefficace
+
+![spawn target](image/image2.png)
+
--- turtle_scanner_gaelle1/turtle_scanner_gaelle1/turtle_scanner_node.py
+++ turtle_scanner_gaelle1/turtle_scanner_gaelle1/turtle_scanner_node.py
@@ -3,6 +3,8 @@
 import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+import math
 
 
 class TurtleScannerNode(Node):
@@ -14,7 +16,10 @@ class TurtleScannerNode(Node):
         self.pose_scanner = None
         self.pose_target = None
 
-        # subscriber turtle1
+        # publisher cmd_vel
+        self.cmd_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+
+        # subscriber scanner
         self.create_subscription(
             Pose,
             '/turtle1/pose',
@@ -22,25 +27,83 @@ class TurtleScannerNode(Node):
             10
         )
 
-        # subscriber turtle_target
-        self.create_subscription(
-            Pose,
-            '/turtle_target/pose',
-            self.target_callback,
-            10
-        )
+        # serpentin
+        self.waypoints = self.generate_serpentin()
+        self.index = 0
 
+        # timer 20 Hz
+        self.timer = self.create_timer(0.05, self.scan_step)
+
+        # gains
+        self.Kp_lin = 1.0
+        self.Kp_ang = 4.0
+        self.tolerance = 0.3
+
+        self.linear_speed_max = 2.0
+
+    # CALLBACK POSE
     def scanner_callback(self, msg):
         self.pose_scanner = msg
-        self.get_logger().info(
-            f"Scanner: x={msg.x:.2f}, y={msg.y:.2f}"
-        )
 
-    def target_callback(self, msg):
-        self.pose_target = msg
-        self.get_logger().info(
-            f"Target: x={msg.x:.2f}, y={msg.y:.2f}"
-        )
+    # SERPENTIN WAYPOINTS
+    def generate_serpentin(self):
+        waypoints = []
+
+        nb_lignes = 5
+        y_start = 1.0
+        y_step = 2.0
+        x_min = 1.0
+        x_max = 10.0
+
+        for i in range(nb_lignes):
+            y = y_start + i * y_step
+
+            if i % 2 == 0:
+                waypoints.append((x_max, y))
+                waypoints.append((x_min, y))
+            else:
+                waypoints.append((x_min, y))
+                waypoints.append((x_max, y))
+
+        return waypoints
+
+    # DISTANCE
+    def compute_distance(self, A, B):
+        return math.sqrt((B[0] - A.x)**2 + (B[1] - A.y)**2)
+
+    # ANGLE
+    def compute_angle(self, A, B):
+        return math.atan2(B[1] - A.y, B[0] - A.x)
+
+    # SCAN STEP
+    def scan_step(self):
+
+        if self.pose_scanner is None or self.index >= len(self.waypoints):
+            if self.index >= len(self.waypoints):
+                self.stop()
+                self.get_logger().info("Balayage terminé")
+            return
+
+        target = self.waypoints[self.index]
+
+        distance = self.compute_distance(self.pose_scanner, target)
+        angle_desired = self.compute_angle(self.pose_scanner, target)
+
+        error_angle = math.atan(math.tan((angle_desired - self.pose_scanner.theta) / 2))
+
+        cmd = Twist()
+
+        if distance < self.tolerance:
+            self.index += 1
+        else:
+            cmd.linear.x = min(self.Kp_lin * distance, self.linear_speed_max)
+            cmd.angular.z = self.Kp_ang * error_angle
+
+        self.cmd_pub.publish(cmd)
+
+    def stop(self):
+        cmd = Twist()
+        self.cmd_pub.publish(cmd)
 
 
 def main(args=None):
